470f5e6523250f1b837bbb5114b7b9f2e2c899bf
Shravan Kumar Boragala <shravan.boragala@compugain.com>
2017-06-13 21:36:42 +0530

    code refractor

--- src/main/java/com/actualize/mortgage/TriDentLeApplication.java
+++ src/main/java/com/actualize/mortgage/TriDentLeApplication.java
@@ -4,14 +4,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 /**
  * This class initiates the current application 
  * @author sboragala
  *
  */
 @SpringBootApplication
-public class TriDentLeApplication extends SpringBootServletInitializer {
+public class TriDentLeApplication {
 	
 	private static final Logger LOG = LogManager.getLogger(TriDentLeApplication.class);
 
